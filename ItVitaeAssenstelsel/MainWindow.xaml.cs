f8167b8fa39507f1f42e557d97081cb771d2d80c
Stella van der burg <luna4s108@gmail.com>
2020-06-15 15:10:01 +0200

    - Added DrawinGrid and supporting methods. That will draw a grid according to with where the middenpunt is placed.

--- ItVitaeAssenstelsel/MainWindow.xaml.cs
+++ ItVitaeAssenstelsel/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace ItVitaeAssenstelsel
         #region private variables
         private bool firstClick = true;
         private Point middenPunt;
+        private int rasterOffSet = 10;
         #endregion
         public MainWindow()
         {
@@ -37,11 +38,19 @@ namespace ItVitaeAssenstelsel
 
         private void CanvasMain_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+           
             Point mousePosCanvas = Mouse.GetPosition(this);
-            if (firstClick)
-            {
+          
+            //if (firstClick)
+           // {
+                //Temporary Canvas remove.
+                if (!firstClick)
+                {
+                    CanvasMain.Children.Clear();
+                }
                 //Set middenPunt
                 middenPunt = mousePosCanvas;
+                DrawGrid(rasterOffSet, middenPunt, CanvasMain);
                 TextBkMiddenP.Text = string.Format("({0}, {1})", 0, 0);
 
                 //Create and add lines to CanvasMain.
@@ -64,15 +73,96 @@ namespace ItVitaeAssenstelsel
                     Y1 = 0,
                     Y2 = SystemParameters.PrimaryScreenHeight
                 };
+                
+              
                 //Add both lines to canvas.
                 CanvasMain.Children.Add(lineHor);
                 CanvasMain.Children.Add(lineVer);
 
                 firstClick = false;
-            }
+            //}
 
             //Set Wiskundige coordinaten. Middenpunt - mouseCanvas positie. 
             TextBkWis.Text = string.Format("({0:0}, {1:0})", middenPunt.X - mousePosCanvas.X, middenPunt.Y - mousePosCanvas.Y);
+        }
+
+        public void DrawGrid(int offSet, Point middenP, Canvas mainCanvas)
+        {
+            RemoveGrid(mainCanvas);
+            Image lines = new Image();
+            lines.SetValue(Panel.ZIndexProperty, -100);
+            //Draw the grid
+            DrawingVisual gridLinesVisual = new DrawingVisual();
+            DrawingContext dct = gridLinesVisual.RenderOpen();
+            Pen darkPen = new Pen(Brushes.DarkOrange, 1), lightPen = new Pen(Brushes.Orange, 0.5);
+            darkPen.Freeze();
+            lightPen.Freeze();
+
+            int yOffset = offSet,
+                xOffset = offSet,
+                rows = (int)(SystemParameters.PrimaryScreenHeight),
+                columns = (int)(SystemParameters.PrimaryScreenWidth),
+                //Alternate makes after each 10 lines the tenth be drawn with darkPen.
+                alternate = 10,
+                j = 0;
+
+            //Draw the horizontal lines
+            Point a = new Point(0, AdjustingPoint(middenP.Y, CanvasMain.ActualHeight));
+            Point b = new Point(SystemParameters.PrimaryScreenWidth, AdjustingPoint(middenP.Y, CanvasMain.ActualHeight));
+
+            for (int i = 0; i <= rows; i++, j++)
+            {
+                dct.DrawLine(j % alternate == 0 ? darkPen : lightPen, a, b);
+                a.Offset(0, yOffset);
+                b.Offset(0, yOffset);
+            }
+            j = 0;
+
+            //Draw the vertical lines
+            a = new Point(AdjustingPoint(middenP.X, CanvasMain.ActualWidth), 0);
+            b = new Point(AdjustingPoint(middenP.X, CanvasMain.ActualWidth), SystemParameters.PrimaryScreenHeight);
+
+            for (int i = 0; i <= columns; i++, j++)
+            {
+                dct.DrawLine(j % alternate == 0 ? darkPen : lightPen, a, b);
+                a.Offset(xOffset, 0);
+                b.Offset(xOffset, 0);
+            }
+
+            dct.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)SystemParameters.PrimaryScreenWidth,
+                (int)SystemParameters.PrimaryScreenHeight, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(gridLinesVisual);
+            bmp.Freeze();
+            lines.Source = bmp;
+
+            mainCanvas.Children.Add(lines);
+        }
+
+        /// <summary>
+        //  To use with making new Points to use with drawingLines in DrawGrid.
+        /// Using rounded actualheight to have the horizontal lines reach from top to bottom canvas without offsetting them from main lines.
+        /// Adding + 0.5 to prevent grid from being blurry.
+        /// </summary>
+        /// <param name="point">Based off the middenpoint for the 0,0 coordinates.</param>
+        /// <param name="Canvas">To send Canvas.actualHeight or actualWidth accordingly.</param>
+        /// <returns></returns>
+        private double AdjustingPoint(double point, double Canvas)
+        {
+            return Math.Round(point - (Math.Round((Canvas / 100)) * 100)) + 0.5;
+        }
+
+        private void RemoveGrid(Canvas mainCanvas)
+        {
+            foreach (UIElement obj in mainCanvas.Children)
+            {
+                if (obj is Image)
+                {
+                    mainCanvas.Children.Remove(obj);
+                    break;
+                }
+            }
         }
     }
 }
