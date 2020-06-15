9d8030cd6da60c1603a0af667d89ef1030d33e00
Stella van der burg <luna4s108@gmail.com>
2020-06-15 11:52:13 +0200

    - Added CanvasMain_MouseLeftButtonUp method. Which will set the middelpunt once and add the horizontal and vertical raster lines. And change the Wiskundige Coordinaten.

--- ItVitaeAssenstelsel/MainWindow.xaml.cs
+++ ItVitaeAssenstelsel/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace ItVitaeAssenstelsel
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region private variables
+        private bool firstClick = true;
+        private Point middenPunt;
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +32,47 @@ namespace ItVitaeAssenstelsel
         private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             Point mousePos = PointToScreen(Mouse.GetPosition(this));
-            TextBkBeeld.Text = string.Format("({0},{1})", mousePos.X, mousePos.Y);
+            TextBkBeeld.Text = string.Format("({0}, {1})", mousePos.X, mousePos.Y);
+        }
+
+        private void CanvasMain_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Point mousePosCanvas = Mouse.GetPosition(this);
+            if (firstClick)
+            {
+                //Set middenPunt
+                middenPunt = mousePosCanvas;
+                TextBkMiddenP.Text = string.Format("({0}, {1})", 0, 0);
+
+                //Create and add lines to CanvasMain.
+                //Start at 0 end at PrimaryScreen Width/Height to ensure the line will cross the entire window.
+                Line lineHor = new Line
+                {
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 1,
+                    X1 = 0,
+                    X2 = SystemParameters.PrimaryScreenWidth,
+                    Y1 = mousePosCanvas.Y,
+                    Y2 = mousePosCanvas.Y
+                };
+                Line lineVer = new Line
+                {
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 1,
+                    X1 = mousePosCanvas.X,
+                    X2 = mousePosCanvas.X,
+                    Y1 = 0,
+                    Y2 = SystemParameters.PrimaryScreenHeight
+                };
+                //Add both lines to canvas.
+                CanvasMain.Children.Add(lineHor);
+                CanvasMain.Children.Add(lineVer);
+
+                firstClick = false;
+            }
+
+            //Set Wiskundige coordinaten. Middenpunt - mouseCanvas positie. 
+            TextBkWis.Text = string.Format("({0:0}, {1:0})", middenPunt.X - mousePosCanvas.X, middenPunt.Y - mousePosCanvas.Y);
         }
     }
 }
